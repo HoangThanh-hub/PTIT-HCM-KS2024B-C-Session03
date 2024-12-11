27910e94d12461738c470aeeab36e32da36b7106
HoangThanh-hub <tranthanh02012006@gmail.com>
2024-12-11 16:40:26 +0700

    Add files via upload

---
+++ session03-baitap03.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float r,c,s;
+    printf("nhap ban kinh r : ");
+    scanf("%f", &r);
+    c = 2*3.14*r;
+    s = 3.14 * (r * r);
+
+    printf("chu vi hinh tron la: %.2f\n", c);
+
+    printf("dien tich hinh tron la: %.2f", s);
+
+
+
+
+
+    return 0;
+}
